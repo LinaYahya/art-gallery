80e384ad0dce9e3badc692eae9a4619bcd78cfac
Lina Ebeid <linaebe0@gmail.com>
2020-10-10 20:53:33 +0300

    remove slider

--- client/src/components/common/Slider/index.js
+++ client/src/components/common/Slider/index.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import { Carousel } from 'antd';
-import pic1 from '../../../assets/images/Calligraphy.jpg';
-import pic3 from '../../../assets/images/Jerusalem.jpg';
-import pic4 from '../../../assets/images/EgyptPyramids.jpg';
 import pic5 from '../../../assets/images/words.jpg';
 import './style.css';
 
@@ -17,20 +13,9 @@ const SlideShow = () => {
         </p>
       </div>
       <div className="carousal-container">
-        <Carousel autoplay>
-          <div className="image-container">
-            <img className="slider-image" src={pic5} alt="words" />
-          </div>
-          <div className="image-container">
-            <img className="slider-image" src={pic1} alt="Calligraphy" />
-          </div>
-          <div className="image-container">
-            <img src={pic3} alt="Jerusalem" className="slider-image" />
-          </div>
-          <div className="image-container">
-            <img src={pic4} alt="EgyptPyramids" className="slider-image" />
-          </div>
-        </Carousel>
+        <div className="image-container">
+          <img className="slider-image" src={pic5} alt="words" />
+        </div>
       </div>
     </div>
   );
